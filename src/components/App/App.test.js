ee36cec888cb2f0aca11e41070764a114890de7c
Jabari Dash <jabari.dash@gmail.com>
2020-05-21 14:52:49 -0500

    updated test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { render } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Here you can grab real estate data about houses for sale./i);
   expect(linkElement).toBeInTheDocument();
 });
